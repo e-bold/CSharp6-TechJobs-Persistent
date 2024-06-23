2619d02bd7737372b3b395632d9c6b4b43c11197
e-bold <enkhmunkh@att.net>
2024-06-23 12:39:32 -0500

    Added Task3 SQL command. Pending demo

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -4,18 +4,29 @@
 
 --Part 1: List the columns and their data types in the Jobs table.
 
--- Table: jobs
--- Columns:
--- Id int AI PK   - integer (auto incremented, primary key)
--- Name longtext   - text (holds 4,294,967,295 characters)
--- EmployerId int  - integer
+/*
+Table: jobs
+Columns:
+Id int AI PK   - integer (auto incremented, primary key)
+Name longtext   - text (holds 4,294,967,295 characters)
+EmployerId int  - integer
+*/
 
 --Part 2: Write a query to list the names of the employers in St. Louis City.
 
--- SELECT Name
--- FROM employers
--- WHERE Location = "St. Louis, MO";
+/*
+SELECT Name
+FROM employers
+WHERE Location = "St. Louis, MO";
+*/
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
-    
+
+/*
+SELECT Name, SkillName 
+FROM jobskill
+JOIN jobs ON jobs.Id = jobskill.JobsId
+JOIN skills ON skills.Id = jobskill.SkillsId
+ORDER BY SkillName;
+*/
